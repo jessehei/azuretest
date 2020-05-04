c1b689bf27f52a67c27e34132a43304932a38d56
jeshe <jesse.hei@elisanet.fi>
2020-05-04 16:02:38 +0300

    Add JUnit testing

---
+++ src/main/java/junit/JUnitMeasure.java
@@ -0,0 +1,8 @@
+package junit;
+
+public class JUnitMeasure {
+
+    public boolean isGreater(int num1, int num2){
+        return num1 > num2;
+    }
+}
---
+++ src/test/java/junit/JUnitMeasureTest.java
@@ -0,0 +1,38 @@
+package junit;
+
+import static org.junit.Assert.assertTrue;
+ 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JUnitMeasureTest {
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+ 
+    @Before
+    public void before() {
+        System.out.println("Before Test Case");
+    }
+ 
+    @Test
+    public void isGreaterTest() {
+        System.out.println("Test");
+        JUnitMeasure measureTest = new JUnitMeasure();
+        assertTrue("Num 1 is greater than Num 2", measureTest.isGreater(4, 3));
+    }
+ 
+    @After
+    public void after() {
+        System.out.println("After Test Case");
+    }
+ 
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
+}
